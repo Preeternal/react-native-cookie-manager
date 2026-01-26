edb1cc66e70fe2dacb282f07098b210853560da6
Preeternal <preeternal@gmail.com>
2026-01-26 20:25:52 +0200

    fix(ios): import RCTBridgeModule only in old arch

--- ios/CookieManager.h
+++ ios/CookieManager.h
@@ -1,9 +1,8 @@
-#import <React/RCTBridgeModule.h>
-
 #if RCT_NEW_ARCH_ENABLED
 #import <CookieManagerSpec/CookieManagerSpec.h>
 @interface CookieManager : NSObject <NativeCookieManagerSpec>
 #else
+#import <React/RCTBridgeModule.h>
 @interface CookieManager : NSObject <RCTBridgeModule>
 #endif
 
